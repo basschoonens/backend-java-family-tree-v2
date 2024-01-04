a5e4fc0c80bbfc3ceba4cd58828341afbd7fddd8
basschoonens <bas.schoonens@gmail.com>
2024-01-04 17:16:23 +0100

    testen toegevoegd-nog invullen

---
+++ src/test/java/org/example/PersonTest.java
@@ -0,0 +1,157 @@
+package org.example;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class PersonTest {
+
+
+    @Test
+    void getName() {
+        // arrange
+//        Person person1 = new Person("Bas","Schoonens","man",42);
+        var person = new Person("Bas","Schoonens","man", 42);
+
+        // act
+//        String name = person1.getName();
+
+        // assert
+//        assertEquals("Bas", name);
+
+    }
+
+    @Test
+    void getMiddleName() {
+        // arrange
+
+        // act
+
+        // assert
+    }
+
+    @Test
+    void getLastName() {
+        // arrange
+
+        // act
+
+        // assert
+    }
+
+    @Test
+    void getSex() {
+        // arrange
+
+        // act
+
+        // assert
+    }
+
+    @Test
+    void getAge() {
+        // arrange
+
+        // act
+
+        // assert
+    }
+
+    @Test
+    void getMother() {
+        // arrange
+
+        // act
+
+        // assert
+    }
+
+    @Test
+    void getFather() {
+        // arrange
+
+        // act
+
+        // assert
+    }
+
+    @Test
+    void getSiblings() {
+        // arrange
+
+        // act
+
+        // assert
+    }
+
+    @Test
+    void getChildren() {
+        // arrange
+
+        // act
+
+        // assert
+    }
+
+    @Test
+    void getPets() {
+        // arrange
+
+        // act
+
+        // assert
+    }
+
+    @Test
+    void addParents() {
+        // arrange
+
+        // act
+
+        // assert
+
+    }
+
+    @Test
+    void addChild() {
+        // arrange
+
+        // act
+
+        // assert
+
+    }
+
+    @Test
+    void addPet() {
+        // arrange
+
+        // act
+
+        // assert
+
+    }
+
+    @Test
+    void addSibling() {
+        // arrange
+
+        // act
+
+        // assert
+
+    }
+
+    @Test
+    void getGrandchildren() {
+        // arrange
+
+        // act
+
+        // assert
+
+    }
+
+}
